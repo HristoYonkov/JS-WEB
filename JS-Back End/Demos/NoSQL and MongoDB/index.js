f3734606cc787fea92d1640fe7971ef72f9ab9d6
Hristo Yonkov <hristopturs@gmail.com>
2022-10-02 19:29:21 +0300

    add initial architecture

---
+++ JS-Back End/Demos/NoSQL and MongoDB/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const app = express();
+
+// ---------------- ADD the database!!!...
+const { MongoClient } = require('mongodb'); // Taking the mongo client trough the driver
+const client = new MongoClient('mongodb://localhost:27017');
+client.connect()
+    .then(() => {
+        console.log('DB connected successfully!');
+    });
+const dB = client.db('Movies');         // Name of database
+const moviesDB = dB.collection('movies');   // Name of collection
+// ---------------- ADD the database!!!...
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/movies', async (req, res) => {
+    let movies = await moviesDB.find().toArray();
+    res.json(movies);
+});
+
+
+app.listen(3000, () => console.log('Server is listening on port 3000'));
