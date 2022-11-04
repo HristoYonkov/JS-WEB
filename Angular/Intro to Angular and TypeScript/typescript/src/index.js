6a946feb17533572f1c5cd621357168c109f7f66
Hristo Yonkov <hristopturs@gmail.com>
2022-11-04 16:19:35 +0200

    add initia larchitecture

---
+++ Angular/Intro to Angular and TypeScript/typescript/src/index.js
@@ -0,0 +1,8 @@
+let isDone = false;
+let decimal = 5;
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
+let message = '';
+let messages = ['message1', 'message2'];
+let strictMessage = ['message1', 'message2'];
---
+++ Angular/Intro to Angular and TypeScript/typescript/src/index.ts
@@ -0,0 +1,11 @@
+let isDone: boolean = false;
+
+let decimal: number = 5;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+let message: string = '';
+
+let messages: string[] = ['message1', 'message2'];
+let strictMessage: ['message1', 'message2'] = ['message1', 'message2'];
