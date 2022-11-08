fa367ab54f96435b99d3d5a02cfeef1708e33b44
Hristo Yonkov <hristopturs@gmail.com>
2022-11-08 21:18:53 +0200

    update example !

--- Angular/Components/component-app/src/app/list-item/list-item.component.ts
+++ Angular/Components/component-app/src/app/list-item/list-item.component.ts
@@ -1,16 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+export interface ICustomEvent {
+    test: number;
+}
 @Component({
-  selector: 'app-list-item',
-  templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.scss']
+    selector: 'app-list-item',
+    templateUrl: './list-item.component.html',
+    styleUrls: ['./list-item.component.scss']
 })
 export class ListItemComponent {
 
-  @Input() user!: { firstName: string, lastName: string }
-  @Input() ifLastName!: boolean;
-  @Input() staticString!: string;
-  @Input() staticNumber!: Number;
+    @Input() user!: { firstName: string, lastName: string }
+    @Input() ifLastName!: boolean;
+    @Input() staticString!: string;
+    @Input() staticNumber!: Number;
 
-  constructor() { }
+    @Output('myCustomEvent') customEvent = new EventEmitter <ICustomEvent>();
+
+    constructor() { }
+
+    selectClick($event: MouseEvent) {
+        $event.stopPropagation();
+        this.customEvent.emit({ test: 123 })
+    }
 }
--- Angular/Components/component-app/src/app/list/list.component.ts
+++ Angular/Components/component-app/src/app/list/list.component.ts
@@ -1,38 +1,59 @@
 import { Component } from '@angular/core';
+import { ICustomEvent } from '../list-item/list-item.component';
 
 const myNumber = 1;
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.scss']
 })
 
 export class ListComponent {
 
-  users = [
-    {
-      firstName: 'Pavel',
-      lastName: 'Dimitrov'
-    },
-    {
-      firstName: 'Hristo',
-      lastName: 'Yonkov'
-    },
-    {
-      firstName: 'Cecka',
-      lastName: 'Kolejkata'
+    users = [
+        {
+            firstName: 'Pavel',
+            lastName: 'Dimitrov'
+        },
+        {
+            firstName: 'Hristo',
+            lastName: 'Yonkov'
+        },
+        {
+            firstName: 'Cecka',
+            lastName: 'Kolejkata'
+        }
+    ];
+
+    myNumber = myNumber;
+
+    ifLastName = false;
+
+    userIndex: null | number = null;
+
+    get showIndex(): boolean {
+        return (this.userIndex === null ? -1 : this.userIndex) >= 0;
     }
-  ];
 
-  myNumber = myNumber;
+    constructor() { }
 
-  ifLastName = false;
+    showLastName(event: MouseEvent) {
+        console.log(event);
+        this.ifLastName = !this.ifLastName;
+    }
 
-  constructor() { }
+    listItemClick(index: number) {
+        if (this.userIndex === index) {
+            this.userIndex = null;
+            return;
+        }
+        this.userIndex = index
+    }
 
-  showLastName(event: MouseEvent) {
-    console.log(event);
-    this.ifLastName = !this.ifLastName;
-  }
+    customEventHandler($event: ICustomEvent) {
+        console.log($event);
+        
+    }
+
 }
