c17b8f7b2659714254e6ebe1df30e65d79c834b1
Hristo Yonkov <hristopturs@gmail.com>
2022-11-19 11:24:42 +0200

    providers example added

--- Angular/4 - Modules and Routing/Modules-And-Routing/src/app/app.component.ts
+++ Angular/4 - Modules and Routing/Modules-And-Routing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Test } from './user/user.module';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Modules-And-Routing';
+
+  constructor (private t: Test) { 
+    console.log(this.t);
+  }
 }
--- Angular/4 - Modules and Routing/Modules-And-Routing/src/app/user/user.module.ts
+++ Angular/4 - Modules and Routing/Modules-And-Routing/src/app/user/user.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 
-
+export class Test {
+}
 
 @NgModule({
-  declarations: [
-    ListComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    ListComponent
-  ]
+    declarations: [
+        ListComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    providers: [
+        Test // { provide: Test, useClass: Test }
+    ],
+    exports: [
+        ListComponent
+    ]
 })
 export class UserModule { }
