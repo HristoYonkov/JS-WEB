6a7f91411d437771154099b06be200aeff206890
Hristo Yonkov <hristopturs@gmail.com>
2022-12-01 12:39:54 +0200

    home login and register components added

--- Angular/5 - Forms/Forms/src/app/app.module.ts
+++ Angular/5 - Forms/Forms/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // --------- For template driven FORMS!
   ],
   providers: [],
   bootstrap: [AppComponent]
