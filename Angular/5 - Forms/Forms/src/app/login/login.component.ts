7b93a4ee2e358726544c9ecbd707235c128b2c96
Hristo Yonkov <hristopturs@gmail.com>
2022-12-01 13:23:48 +0200

    add login foem submit handler

--- Angular/5 - Forms/Forms/src/app/login/login.component.ts
+++ Angular/5 - Forms/Forms/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,13 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleFormSubmit(form: NgForm) {
+    if (form.invalid) { return; }
+    const value: { email: string, password: string } = form.value
+    form.setValue({email: '', password: ''})
+    console.log(value);
   }
 
 }
