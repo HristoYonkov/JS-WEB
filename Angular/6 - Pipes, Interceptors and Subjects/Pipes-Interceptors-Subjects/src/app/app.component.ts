71ebb36e1ca421570f5d5c3aa8c6412b697882ac
Hristo Yonkov <hristopturs@gmail.com>
2022-12-04 22:08:55 +0200

    update examples!

--- Angular/6 - Pipes, Interceptors and Subjects/Pipes-Interceptors-Subjects/src/app/app.component.ts
+++ Angular/6 - Pipes, Interceptors and Subjects/Pipes-Interceptors-Subjects/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipes-Interceptors-Subjects';
+
+  obj = {
+    scores: [1, 2, 3, 4, 5, 6, 9],
+  }
+
+  private scores: number[] = [];
+  private result = 0;
+
+  calcScores(obj: { scores: number[] }) {
+    if (this.scores !== obj.scores) {
+      this.result = obj.scores.reduce((acc, curr) => acc + curr)
+      this.scores = obj.scores;
+    }
+    return obj.scores.reduce((acc, curr) => acc + curr);
+  }
+
+  addProp() {
+    (this.obj as any)['test'] = 500;
+  }
+
 }
