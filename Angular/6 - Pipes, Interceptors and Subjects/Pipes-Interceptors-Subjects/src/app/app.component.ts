d000d5df8de3b87ef8b1ba4154606add1b1f7f9c
Hristo Yonkov <hristopturs@gmail.com>
2022-12-05 00:45:34 +0200

    updates example

--- Angular/6 - Pipes, Interceptors and Subjects/Pipes-Interceptors-Subjects/src/app/app.component.ts
+++ Angular/6 - Pipes, Interceptors and Subjects/Pipes-Interceptors-Subjects/src/app/app.component.ts
@@ -1,30 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { interval, map } from 'rxjs';
+
+function add(a: number | string, b: number | string) {
+    return (a) as any + (b) as any;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    // Default Change detection, (only if refferences are changed!) "'Default' is checking always"
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'Pipes-Interceptors-Subjects';
+    title = 'Pipes-Interceptors-Subjects';
 
-  obj = {
-    scores: [1, 2, 3, 4, 5, 6, 9],
-  }
-
-  private scores: number[] = [];
-  private result = 0;
-
-  calcScores(obj: { scores: number[] }) {
-    if (this.scores !== obj.scores) {
-      this.result = obj.scores.reduce((acc, curr) => acc + curr)
-      this.scores = obj.scores;
+    obj = {
+        scores: [1, 2, 3, 4, 5, 6, 9],
     }
-    return obj.scores.reduce((acc, curr) => acc + curr);
-  }
 
-  addProp() {
-    (this.obj as any)['test'] = 500;
-  }
+    add = add;
+
+    private scores: number[] = [];
+    private result = 0;
+
+    $time = interval(1000).pipe(
+        map(() => new Date())
+    );
+
+    myPromise = new Promise((res) => {
+        setTimeout(() => {
+            res('Hello World!')
+        }, 3000)
+    })
+
+    calcScores(obj: { scores: number[] }) {
+        if (this.scores !== obj.scores) {
+            this.result = obj.scores.reduce((acc, curr) => acc + curr)
+            this.scores = obj.scores;
+            return this.result;
+        }
+        return this.result;
+    }
+
+    addProp() {
+        (this.obj as any)['test'] = 500;
+        this.obj.scores = this.obj.scores.concat(100);
+    }
 
 }
--- Angular/6 - Pipes, Interceptors and Subjects/Pipes-Interceptors-Subjects/src/app/reduce.pipe.ts
+++ Angular/6 - Pipes, Interceptors and Subjects/Pipes-Interceptors-Subjects/src/app/reduce.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'reduce'
+  name: 'reduce',
+  pure: true // Default Change detection, (only if refferences are changed!) "'false' is checking always"
 })
-export class ReducePipe implements PipeTransform {
+export class ReducePipe<T, I> implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(array: T[], reduceFn: (acc: any, curr: T) => any, initialValue: I): any {
+    if (initialValue !== undefined) {
+      return array?.reduce(reduceFn, initialValue);
+    }
+    return array?.reduce(reduceFn);
   }
 
 }
