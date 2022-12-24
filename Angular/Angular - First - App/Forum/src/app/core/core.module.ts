bd5d3753634e73c28aac821bc6af8dfe3cb0e646
Hristo Yonkov <hristopturs@gmail.com>
2022-12-24 14:07:18 +0200

    Update core.module.ts

--- Angular/Angular - First - App/Forum/src/app/core/core.module.ts
+++ Angular/Angular - First - App/Forum/src/app/core/core.module.ts
@@ -7,8 +7,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 
-
-
 @NgModule({
   declarations: [
     HeaderComponent,
